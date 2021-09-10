7576a4b5c6928c8b3964c76a4dddcf56ad71ae7b
adrielmoreno <69350702+adrielmoreno@users.noreply.github.com>
2021-09-10 20:37:47 +0200

    Add files via upload

---
+++ FullStack02_JavaScript_Esencial/12 - Trabajando con arreglos/1 - Creando tu primer arreglo/js/app.js
@@ -0,0 +1,11 @@
+"use strict"
+
+//********************************
+//*** Creando tu primer arreglo
+
+var platillos = [ "ceviche", "tacos", "pasta"];
+
+var bebidas = new Array( "Jamaica", "Chicha Morada", "Pozol" );
+
+console.log(typeof platillos, bebidas)
+console.log(Array.isArray(platillos), bebidas)
---
+++ FullStack02_JavaScript_Esencial/12 - Trabajando con arreglos/4 - Operaciones básicas de un arreglo/js/app.js
@@ -0,0 +1,21 @@
+"use strict"
+
+//********************************
+//*** Operaciones básicas de un arreglo
+
+var platillos = ["ceviche", "tacos", "pasta"];
+
+console.log('Antes:', platillos);
+
+//platillos.push("Tostadas");
+
+//platillos.push("Queso");
+
+// platillos.pop();
+// platillos.pop();
+
+//une los valores dentro del arreglo separados por coma
+var mensaje = platillos.join();
+console.log( mensaje);
+
+//console.log('Después:', platillos);
